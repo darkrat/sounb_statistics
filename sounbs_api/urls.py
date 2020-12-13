cf70d1633696bb6e1a05d717a613a8af2141ef55
darkrat <icevirm@gmail.com>
2020-12-13 17:30:32 +0500

    fix url

--- sounbs_api/urls.py
+++ sounbs_api/urls.py
@@ -3,9 +3,9 @@ from .views import *
 app_name = "sounbs_api"
 # app_name will help us do a reverse look-up latter.
 router = DefaultRouter()
-router.register(r'departments', DepartmentView, basename='user')
-router.register(r'themes', ThemeView, basename='user')
-router.register(r'eventtypes', EventTypeView, basename='user')
-router.register(r'eventforms', EventFormView, basename='user')
-router.register(r'events', EventView, basename='user')
+router.register(r'departments', DepartmentView)
+router.register(r'themes', ThemeView)
+router.register(r'eventtypes', EventTypeView)
+router.register(r'eventforms', EventFormView)
+router.register(r'events', EventView)
 urlpatterns = router.urls
