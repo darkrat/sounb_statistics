1b125a4ba98651445c18a310c3b432f206bb0fa4
darkrat <icevirm@gmail.com>
2020-12-13 15:06:56 +0500

    add api departments

--- sounbs_api/views.py
+++ sounbs_api/views.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Department
 # Create your views here.
+
+class DepartmentView(APIView):
+    def get(self, request):
+        departments = Department.objects.all()
+        return Response({"departments": departments})
