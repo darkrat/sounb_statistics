792e5fde498018f6ace03376db39d73702c85d48
darkrat <icevirm@gmail.com>
2020-12-13 16:33:57 +0500

    new router

--- sounbs_api/urls.py
+++ sounbs_api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from .views import *
 app_name = "sounbs_api"
 # app_name will help us do a reverse look-up latter.
-urlpatterns = [
-    path('departments/', DepartmentView.as_view()),
-    path('themes/', ThemeView.as_view()),
-    path('evettypes/', EventTypeView.as_view()),
-    path('eventforms/', EventFormView.as_view()),
-    path('events/', EventView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'departments', DepartmentView, basename='user')
+router.register(r'themes', ThemeView, basename='user')
+router.register(r'eventtypes', EventTypeView, basename='user')
+router.register(r'eventforms', EventFormView, basename='user')
+router.register(r'events', EventView, basename='user')
+urlpatterns = router.urls
