147e308713e53b7208241d05d78a8c994eb1ae82
darkrat <icevirm@gmail.com>
2020-12-13 13:18:53 +0500

    fix allowed hosts

--- sounb_statistics/settings.py
+++ sounb_statistics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bpt)o#f7xw@r(tvgbx_wu^*t4=4dx&k%g)#i-t6e#2bqr3smc4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [192.168.1.109]
+ALLOWED_HOSTS = ['192.168.1.109']
 
 
 # Application definition
