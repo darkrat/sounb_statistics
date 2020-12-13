7550c636b1698a165680ad6201bfcf941cd05016
darkrat <icevirm@gmail.com>
2020-12-13 15:11:00 +0500

    fix

--- sounb_statistics/urls.py
+++ sounb_statistics/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path(r'^api/', include('sounds_api.urls')),
+    url(r'^api/', include('sounds_api.urls')),
 ]
