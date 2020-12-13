e5f39e8816592659eae610f9bd77e9954b882b54
darkrat <icevirm@gmail.com>
2020-12-13 14:30:15 +0500

    register models

--- sounbs_api/admin.py
+++ sounbs_api/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import EventType, EventForm, Department, Theme, Event
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(EventForm)
+admin.site.register(EventType)
+admin.site.register(Department)
+admin.site.register(Theme)
+
