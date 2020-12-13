d8da383606b5f296625d6b6c3ce644a23c18f18c
darkrat <icevirm@gmail.com>
2020-12-13 14:23:34 +0500

    fix models

--- sounbs_api/models.py
+++ sounbs_api/models.py
@@ -18,11 +18,11 @@ class Theme(models.Model):
     title = models.CharField(max_length=255)
 
 class Event(models.Model):
-    EventType = models.ForeignKey('EventType', models.SET_NULL, verbose_name='Тип мероприятия',help_text='Массовое мероприятие')
-    Department =models.ForeignKey('Department', models.SET_NULL, verbose_name='Отдел-организатор',help_text='Выберите из списка')
-    EventForm = models.ForeignKey('EventForm', models.SET_NULL, verbose_name='',help_text='Выберите из списка')
-    Owner = models.ForeignKey(get_user_model(), models.SET_NULL, verbose_name='Оператор')
-    Theme = models.ForeignKey('Theme', models.SET_NULL, verbose_name='Тема мероприятия',help_text='Выберите из списка')
+    EventType = models.ForeignKey('EventType', on_delete=models.CASCADE, verbose_name='Тип мероприятия',help_text='Массовое мероприятие')
+    Department =models.ForeignKey('Department', on_delete=models.CASCADE, verbose_name='Отдел-организатор',help_text='Выберите из списка')
+    EventForm = models.ForeignKey('EventForm', on_delete=models.CASCADE, verbose_name='',help_text='Выберите из списка')
+    Owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Оператор')
+    Theme = models.ForeignKey('Theme', on_delete=models.CASCADE, verbose_name='Тема мероприятия',help_text='Выберите из списка')
     ExpositionTitle = models.CharField(max_length=255, verbose_name='Название сопровождающей выставки',help_text='Введите название выставки')
     HasPoster = models.BooleanField(default=False, verbose_name='Была ли подготовлена афиша?')
     HasPhotos = models.BooleanField(default=False, verbose_name='Место хранения медиа-отчета') # TODO: что тут? строка или полу есть ли фото???
