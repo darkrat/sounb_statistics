9a1acdebbd541af1221a9ee3d7750217e342473f
darkrat <icevirm@gmail.com>
2021-01-06 19:01:25 +0500

    .

--- sounbs_api/urls.py
+++ sounbs_api/urls.py
@@ -8,5 +8,5 @@ router.register(r'themes', ThemeView)
 router.register(r'eventtypes', EventTypeView)
 router.register(r'eventforms', EventFormView)
 router.register(r'events', EventView)
-router.register(r'metaevent', EventTitleView)
+router.register(r'metaevent', EventTitleView.as_view())
 urlpatterns = router.urls
