988793439cf17965c8591e760eb7d61188042b5c
darkrat <icevirm@gmail.com>
2021-01-06 17:26:03 +0500

    add bootstrap template

--- sounbs_operator/views.py
+++ sounbs_operator/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'index.html', {})
+    data = {"app_title": "Статистика", "username": "Пользователь"}
+    return render(request, 'index.html', context=data)
