756f87b6722130661b64c8788719b935a8c26700
darkrat <icevirm@gmail.com>
2021-01-10 01:01:11 +0500

    .

--- sounbs_api/serializers.py
+++ sounbs_api/serializers.py
@@ -6,7 +6,7 @@ class ListSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 class DictionarySerializer(serializers.Serializer):
-    id = serializers.IntegerField
+    id = serializers.Field()
     title = serializers.CharField(max_length=255)
 
 class EventSerializer(serializers.ModelSerializer):
