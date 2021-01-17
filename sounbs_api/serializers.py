72a42b6ed6ca56a8a057204c3550a6d71f7031e8
darkrat <icevirm@gmail.com>
2021-01-17 15:46:39 +0500

    .

--- sounbs_api/serializers.py
+++ sounbs_api/serializers.py
@@ -40,8 +40,8 @@ class EventSerializer(serializers.ModelSerializer):
         model = Event
         fields = ('id', 'Theme_id', 'ExpositionTitle', 'HasPoster', 'HasPhotos', 'StartDate', 'EndDate', 'Title', 'Description', 'Author', 'Location', 'VisitorCount', 'ChildVisitorCount', 'JuniorVisitorCount', 'BooksOnDisplayCount', 'IssuedBooksCount', 'Department_id', 'EventForm_id', 'EventType_id', 'Owner_id')
     
-    # def create(self, validated_data):
-    #     print( validated_data)
+    def create(self, validated_data):
+        print(self.CurrentUserDefault())
     #     print(self)
     #     id_department = validated_data.pop('Department_id')
     #     print('create event: id_department=',id_department)
