a6d196fd397f52d47639008f787672c81b74634a
darkrat <icevirm@gmail.com>
2021-01-17 15:30:49 +0500

    .

--- sounbs_api/serializers.py
+++ sounbs_api/serializers.py
@@ -31,11 +31,7 @@ class ThemeSerializer(serializers.ModelSerializer):
 #        depth = 1
 
 class EventSerializer(serializers.ModelSerializer):
-#    Department_id = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all()) # serializers.CharField(source='Department.id', read_only=True)
-#    Theme_id = serializers.PrimaryKeyRelatedField(queryset=Theme.objects.all()) # serializers.CharField(source='Theme.id', read_only=True)
-#    EventForm_id = serializers.PrimaryKeyRelatedField(queryset=EventForm.objects.all()) # serializers.CharField(source='EventForm.id', read_only=True)
-#    EventType_id = serializers.PrimaryKeyRelatedField(queryset=EventType.objects.all()) # serializers.CharField(source='EventType.id', read_only=True)
-#    Owner_id = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Event
         fields = ('id', 'Theme_id', 'ExpositionTitle', 'HasPoster', 'HasPhotos', 'StartDate', 'EndDate', 'Title', 'Description', 'Author', 'Location', 'VisitorCount', 'ChildVisitorCount', 'JuniorVisitorCount', 'BooksOnDisplayCount', 'IssuedBooksCount', 'Department_id', 'EventForm_id', 'EventType_id', 'Owner_id')
